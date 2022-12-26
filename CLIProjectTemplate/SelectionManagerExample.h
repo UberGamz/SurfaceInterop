4cb53a7dec1d1a092d6b659bcf2ace9a574453de
Ubergamz <ubergamz@gmail.com>
2022-12-26 13:18:10 -0500

    Finished

--- CLIProjectTemplate/SelectionManagerExample.h
+++ CLIProjectTemplate/SelectionManagerExample.h
@@ -4,86 +4,107 @@
 // <summary>Declares the SelectionManager class</summary>
 
 // This class demonstrates using Mastercam NET-Hook API and a C++ methods from a C++/CLI project.
-
+#include "m_mill.h"
+#pragma comment(lib,"MCMill.lib")
 #pragma once
 
 namespace Mastercam::IO::Interop {
 
-using namespace System;
-using namespace System::Collections::Generic;
+	using namespace System;
+	using namespace System::Collections::Generic;
 
-// Namespaces we reference in the NET-Hook API
-using namespace Mastercam::App::Types;
-using namespace Mastercam::Database;
-using namespace Mastercam::Database::Types;
-using namespace Mastercam::IO;
-using namespace Mastercam::IO::Types;
+	// Namespaces we reference in the NET-Hook API
+	using namespace Mastercam::App::Types;
+	using namespace Mastercam::Database;
+	using namespace Mastercam::Database::Types;
+	using namespace Mastercam::IO;
+	using namespace Mastercam::IO::Types;
 
-/// <summary> The SelectionManager class. </summary>
-public ref class SelectionManager
+	/// <summary> The SelectionManager class. </summary>
+	public ref class SelectionManager
 	{
-public:
-	/// <summary> This method create a geometry (circle). </summary>
-	///
-	/// <returns> ID of created geometry. </returns>
-	static int SelectionManager::CreateGeometry ();
+	public:
+		/// <summary> This method create a geometry (circle). </summary>
+		///
+		/// <returns> ID of created geometry. </returns>
+		static int SelectionManager::CreateGeometry();
 
-	/// <summary> This method retrieves the entity of the geometry and do a translation. </summary>
-	///
-	/// <param name="geom"> The geometry selected by the user. </param>
-	///
-	/// <returns> True if it succeeds, false if it fails. </returns>
-	//static bool SelectionManager::TranslateSelectedGeometry (int geometryId);
+		/// <summary> This method retrieves the entity of the geometry and do a translation. </summary>
+		///
+		/// <param name="geom"> The geometry selected by the user. </param>
+		///
+		/// <returns> True if it succeeds, false if it fails. </returns>
+		//static bool SelectionManager::TranslateSelectedGeometry (int geometryId);
 
-	/// <summary> This method allows to move the entity of the geometry selected. </summary>
-	///
-	/// <returns> True if it succeeds, false if it fails. </returns>
-	static bool SelectionManager::MoveEntity (ent *entity);
+		/// <summary> This method allows to move the entity of the geometry selected. </summary>
+		///
+		/// <returns> True if it succeeds, false if it fails. </returns>
+		static bool SelectionManager::MoveEntity(ent* entity);
 
-	static System::Collections::Generic::List<int>^ GetOperations(int opCode)
-	{
-
-		auto list = gcnew System::Collections::Generic::List<int>();
-		for (auto index = 0; index < TpMainOpMgr.GetMainOpList().GetSize(); ++index)
+		static System::Collections::Generic::List<int>^ GetOperations(int opCode)
 		{
-			auto op = TpMainOpMgr.GetMainOpList()[index];
-			if (op && (opCode == TP_NULL || op->opcode == opCode))
+
+			auto list = gcnew System::Collections::Generic::List<int>();
+			for (auto index = 0; index < TpMainOpMgr.GetMainOpList().GetSize(); ++index)
 			{
-				list->Add(op->op_idn);
+				auto op = TpMainOpMgr.GetMainOpList()[index];
+				if (op && (opCode == TP_NULL || op->opcode == opCode))
+				{
+					list->Add(op->op_idn);
+				}
 			}
+
+			return list;
 		}
+		static bool SelectionManager::AlterContourFinish(double maxStepdown, double minDepth, double maxDepth)
+		{
 
-		return list;
-	};
-	static bool SelectionManager::AlterContourFinish(long opID, double maxStepdown, double minDepth, double maxDepth)
-	{
-		//Get
-		prm_srf_common opCommon;
-		prm_srf_depths opDepth;
-		tp_entity thisToolPath;
-		thisToolPath.op_idn = opID;
-
-		//Set
-		opCommon.prm_srf_common::max_stepdown = maxStepdown;
-		opDepth.prm_srf_depths::min_depth = minDepth;
-		opDepth.prm_srf_depths::max_depth = maxDepth;
-
-		return true;
-	};
-	static bool SelectionManager::AlterPencilFinish(long opID, bool status, double limitOne, double limitTwo)
-	{
-		//Get
-		prm_srf_limits opData;
-		tp_entity thisToolPath;
-		thisToolPath.op_idn = opID;
-
-		//Set
-		opData.prm_srf_limits::on = status;
-		opData.prm_srf_limits::z1 = limitOne;
-		opData.prm_srf_limits::z2 = limitTwo;
-
-		return true;
-	};
-	};
+			for (auto index = 0; index < TpMainOpMgr.GetMainOpList().GetSize(); ++index)
+			{
+				auto op = TpMainOpMgr.GetMainOpList()[index];
+				if (op && (op->opcode == TP_SRF_FIN_CONTOUR
+					) && op->db.select_flag == true)
+				{
+					bool success = false;
+					DB_LIST_ENT_PTR operationPointer = nullptr;
+					//set cut method to zig zag
+					op->u.r_contour.common.max_stepdown = maxStepdown;
+					op->u.f_contour.depth.max_depth = maxDepth;
+					op->u.f_contour.depth.min_depth = minDepth;
+					operation_manager(op, OPMGR_REWRITE, &operationPointer, &success);
+					if (success == false)
+					{
+						//TO DO : Handle any errors rewriting the operation
+					}
+				}
+			}
+			return true;
+		}
+		static bool SelectionManager::AlterPencilFinish(bool status, double limitOne, double limitTwo)
+		{
+			for (auto index = 0; index < TpMainOpMgr.GetMainOpList().GetSize(); ++index)
+			{
+				auto op = TpMainOpMgr.GetMainOpList()[index];
+				if (op && (op->opcode == TP_SRF_FIN_PENCIL
+					) && op->db.select_flag == true)
+				{
+					bool success = false;
+					DB_LIST_ENT_PTR operationPointer = nullptr;
+					//set cut method to zig zag
+					op->u.f_pencil.limit.on = status;
+					op->u.f_pencil.limit.z1 = limitOne;
+					op->u.f_pencil.limit.z2 = limitTwo;
+					operation_manager(op, OPMGR_REWRITE, &operationPointer, &success);
+					if (success == false)
+					{
+						//TO DO : Handle any errors rewriting the operation
+					}
+				}
+			}
+			return true;
+		};
+	}
+	;
 }
+
 
